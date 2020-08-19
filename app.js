430a1678262df3d8a0c19c5e33a1add3d228b713
wlee004 <wlee004@citymail.cuny.edu>
2020-08-19 17:20:12 -0400

    added student and class json file

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-var db = new PouchDB('todos');
+var db = new PouchDB('todo');
 var remoteCouch = false;
+
+function addTodo(text) {
+    var todo = {
+      _id: new Date().toISOString(),
+      title: text,
+      completed: false
+    };
+    db.put(todo, function callback(err, result) {
+      if (!err) {
+        console.log('Successfully posted a todo!');
+      }
+    });
+  }
