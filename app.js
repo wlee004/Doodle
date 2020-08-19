9efd82eb7972d6322d90392bba01101e3cb44514
Patrick Chaca <40473502+ChacaPatrick@users.noreply.github.com>
2020-08-19 17:58:05 -0400

    Added Json

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-var db = new PouchDB('todo');
-var remoteCouch = false;
+"use strict";
 
-function addTodo(text) {
-    var todo = {
-      _id: new Date().toISOString(),
-      title: text,
-      completed: false
-    };
-    db.put(todo, function callback(err, result) {
-      if (!err) {
-        console.log('Successfully posted a todo!');
-      }
-    });
-  }
+fetch("./classes.json").then(function(resp){
+        return resp.json();
+}).then(function(data){
+    console.log(data);
+})
