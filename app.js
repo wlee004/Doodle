2ba5a89d36a8e16f439fcb45bc7f317d9ce0524b
Patrick Chaca <40473502+ChacaPatrick@users.noreply.github.com>
2020-08-19 19:01:35 -0400

    Fix

--- app.js
+++ app.js
@@ -3,10 +3,5 @@ var student;
 fetch("./students.json").then(function(resp){
         return resp.json();
 }).then(function(data){
-<<<<<<< HEAD
     student=data;
-=======
-    var student = data;
-    console.log(student.name);
->>>>>>> 1632d374d6c46e0abd0a0ffb7279ead3b38d52df
 })
